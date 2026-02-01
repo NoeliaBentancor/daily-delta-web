1207656145378c9fa60c937ff9310cb786059405
Noelia <noeliabentancor1@gmail.com>
2026-02-01 13:48:07 -0300

    Add dummy login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './features/auth/components/auth.component';
+import { HomeLayoutComponent } from '../core/home-layout/home-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: HomeLayoutComponent },
+    { path: "auth", component: AuthComponent }
+];
---
+++ src/app/features/auth/components/auth.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import { Router } from "@angular/router";
+import { InputTextModule } from 'primeng/inputtext';
+@Component({
+    selector: "app-auth",
+    templateUrl: "./auth.component.html",
+    imports: [InputTextModule, ReactiveFormsModule],
+})
+export class AuthComponent {
+    private readonly formBuilder = inject(FormBuilder);
+    private readonly routerService = inject(Router);
+
+    loginForm = this.formBuilder.group({
+        email: [''],
+        password: ['']
+    });
+
+    login() {
+        if (this.loginForm.valid) {
+            const { email, password } = this.loginForm.value;
+            this.routerService.navigate(['/home']);
+            //TODO: Implement authentication logic
+        }
+    }
+
+}
